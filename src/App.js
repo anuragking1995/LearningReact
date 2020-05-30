d5f2c7e00ec90f9d09fb4a8e2523d7fdfa451137
Anurag Shekhawat <anuragshekhawat1995@gmail.com>
2020-05-30 19:02:19 +0530

    L10 state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import Message from './components/Message'
 class App extends Component{
   render() {
     return(
       <div className = "App">
-        <Greet name = "Bruce" heroName = "Batman">
+        <Message></Message>
+        {/* <Greet name = "Bruce" heroName = "Batman">
           <p>This is children props</p>
         </Greet>
         <Greet name = "Clark" heroName = "SuperMan" >
@@ -17,7 +19,7 @@ class App extends Component{
         <Greet name = "Diana" heroName = "Wonder Woman "/>
         <Welcome name = "Bruce" heroName = "Batman"></Welcome>
         <Welcome name = "Clark" heroName = "SuperMan"></Welcome>
-        <Welcome name = "Diana" heroName = "Wonder Woman"></Welcome>
+        <Welcome name = "Diana" heroName = "Wonder Woman"></Welcome> */}
 
       </div>
     )
