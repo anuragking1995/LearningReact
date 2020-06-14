286715ff42f09903f054cb1c261a81dfa10d572d
Anurag Shekhawat <anuragshekhawat1995@gmail.com>
2020-06-14 13:05:53 +0530

    index as anti pattern

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Person from './Person'
 
 function NameList() {
+    const names = ['Bruce','Clark','Diana','Clark']
     const persons = [
         {
            id:1,
@@ -22,10 +23,15 @@ function NameList() {
             skill:'angular'
          }
     ]
-    const personList = persons.map(person =><Person person = {person}/>)
-    return (
-        <div>{personList}</div>
-    )
+    // const personList = persons.map(person =><Person key = {person.id} person = {person}/>)
+    // return (
+    //     <div>{personList}</div>
+    // )
+    //when to use index a skey
+    //the items in your list do not have a unique id . The list is static list and will not change , the list will be never re-ordered or filtered
+    
+const nameList = names.map((name,index) => <h2 key = {index}> { index} {name}</h2>)
+    return <div>{nameList}</div>
 }
 
 export default NameList
--- src/components/Person.js
+++ src/components/Person.js
@@ -5,7 +5,7 @@ function Person({person}) {
         <div>
             <h2>
         I am {person.name}. I am {person.age} years old. I know {person.skill}
-    </h2>
+    </h2>   
         </div>
     )
 }
