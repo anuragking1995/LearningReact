bd9c5e5a557aa00ac34a5b70bbe329a21987639f
Anurag Shekhawat <anuragshekhawat1995@gmail.com>
2020-06-14 14:04:28 +0530

    Css

--- src/App.js
+++ src/App.js
@@ -12,11 +12,21 @@ import EventBind from './components/EventBind'
 import  ParentComponent from './components/ParentComponent'
 import  UserGreeting from './components/UserGreeting'
 import  NameList from './components/NameList'
+import  Stylesheet from './components/Stylesheet'
+import  Inline from './components/Inline'
+import './components/appStyles.css'
+import styles from './components/appStyles.module.css'
+
 class App extends Component{
   render() {
     return(
       <div className = "App">
-        <NameList/>
+        <h1 className = 'error'>Error</h1>
+        <h1 className = {styles.success}>Success</h1>
+        <Inline/>
+         {/* <Inline/> */}
+         {/* <Stylesheet primary = {true}/> */}
+        {/* <NameList/> */}
         {/* <UserGreeting/> */}
         {/* <ParentComponent/> */}
         {/* <EventBind/> */}
